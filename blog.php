507652a32535092ae7741f6e656dda8852ca40a3
jaylord2000 <159142497+jaylord2000@users.noreply.github.com>
2024-02-06 21:52:11 +0800

    Add files via upload

---
+++ blog.php
@@ -0,0 +1,93 @@
+<?php
+session_start();
+
+?>
+
+
+
+
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE-edge">
+    <meta name="viewport" content="width=device-width, intial-scale=1.0, maximum-scale=1.0, user-scalable=no">
+    <title>Blog Section</title>
+    <link rel="stylesheet" type="text/css" href="blog.css"/>
+    <link rel="shortcut icon" href="images/fav-icon.svg"/>
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
+    </head>
+    <body>
+        <section id="blog">
+            <div class="blog-heading">
+              <span>JAYLORD P. INFANTE</span>
+              <h3>My Blog</h3>
+            </div>
+      
+      
+            <div class="blog-container">
+      
+              <div class="blog-box">
+      
+
+                <div class="blog-img">
+                  <img alt="blog" src="picture/professor.webp">
+                </div>
+
+                <div class="blog-text">
+                  <span>This is The Professor" Boucher is an American professional streetball player</span>
+                  <a href="#" class="blog-title">Grayson Scott </a>
+                  <p>Grayson Scott "The Professor" Boucher is an American professional streetball player. He is most known for playing on the highly stylized, international AND1 Mixtape Tour; he has also appeared in several movies including, Semi-Pro, Ball Don't Lie, and Hustle and he is a playable character in several video games</p>
+                  <a href="professor.html">Read More</a>
+                </div>
+      
+              </div>
+      
+      
+      
+      
+   
+              <div class="blog-box">
+      
+                <div class="blog-img">
+                  <img alt="blog" src="picture/tokyo.jpg">
+                </div>
+                <div class="blog-text">
+                  <span>My Favorite Anime</span>
+                  <a href="#" class="blog-title">TOKYO MANJI GANG</a>
+                  <p>The Tokyo Manji Gang, also known as Toman, is a biker gang based in Tokyo, more precisely Shibuya in the World of Tokyo Revengers. It is the main focus of the Tokyo Revengers series, and was founded by five friends, namely Mikey, Draken, Mitsuya, Baji, Kazutora, and Hayashida</p>
+                  <a href="professor.html">Read More</a>
+                </div>
+      
+              </div>
+      
+      
+      
+      
+              
+              <div class="blog-box">
+      
+                
+                <div class="blog-img">
+                  <img alt="blog" src="picture/lamea.jpg">
+                </div>
+                
+                <div class="blog-text">
+                  <span>favorite foods</span>
+                  <a href="#" class="blog-title">Shumai</a>
+                  <p> Shumai is a type of traditional Chinese dumpling. In Cantonese cuisine, it is usually served as a dim sum snack. In addition to accompanying the Chinese diaspora, a variation of shumai also appears in Japan as, various southeast Asian countries and Australia as the dim sim.</p>
+                  <a href="professor.html">Read More</a>
+                </div>
+      
+              </div>
+      
+            </div>
+      
+          </section>
+          
+    </body>
+    </html>
---
+++ config.php
@@ -0,0 +1,16 @@
+<?php
+// Database configuration
+$servername = "localhost";
+$username = "root";
+$password = ""; // Password for MySQL, leave empty if no password is set
+$database = "signup"; // Change this to your database name
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $database);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+?>
+
